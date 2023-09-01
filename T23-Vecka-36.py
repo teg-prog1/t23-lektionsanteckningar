cff5f3161140b9fe97dd454a220bd499b799301d
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2023-09-01 08:58:54 +0200

    outline för genomgången

---
+++ T23-Vecka-36.py
@@ -0,0 +1,28 @@
+# if, else, elif
+
+# if, else och elif kan användas för att programmet ska göra olika saker beroende på villkor
+# indenteringar
+
+# <=, <, >, >=, ==, !=, True
+
+# and, or, not
+
+# nästlade if-satser
+
+
+#########################################################################################################
+
+
+# while-loop
+
+# något måste avbryta loopen
+
+# while, else
+
+# for-loop
+
+# for x in "apple"
+
+# for x in range
+
+# nästlade for-loopar
