3f4ddbef7b7465335614c5c54ebec046ce4ba5bb
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2023-09-12 10:19:00 +0200

    Grund för genomgång uppgift vecka 37

---
+++ T23-Vecka-37.py
@@ -0,0 +1,11 @@
+# Skriv ett program som frågar användaren efter ett ord som är 3-6 bokstäver långt. 
+# När användaren har skrivit in ordet ska programmet skriva ut varje bokstav i ordet på en egen rad.
+
+# Om användaren skriver in ett ord som är för långt eller för kort ska programmet säga ifrån
+# och ge användaren ett nytt försök.
+
+# När ordet är utskrivet ska programmet tala om att det är klart.
+# Användaren ska sedan få en till chans att skriva in ett nytt ord.
+
+
+# ETT STEG I TAGET
