0edb379bf7b9cd5d03731dc84e433377985219df
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2023-09-19 09:20:41 +0200

    grund för genomgång vecka 38

---
+++ T23-Vecka-38.py
@@ -0,0 +1,43 @@
+# Listor, tuples, mängder, dictionarys
+
+# Listor
+# Kan lagra flera värden i en variabel
+
+# Listor är ordnade, det vill säga ordningen spelar roll
+# De är indexerade
+
+# Listor går att förändra värdena på
+
+# Det kan finnas dubletter i listor
+
+# Det går att spara olika datatyper i en lista
+
+# Vi kan loopa igenom en lista
+
+# Vi kan plocka ut längden på en lista
+
+# Metoder för listor: https://www.w3schools.com/python/python_lists_methods.asp
+
+# sort(), append(), pop(), remove(), reverse()
+
+#################################################################################################
+
+# Tuples
+
+# Som listor, men de går inte att ändra
+
+# Mer minneseffektiva och tidseffektiva
+
+# Bättre att använda om man har värden som inte kommer att ändras
+
+#################################################################################################
+
+# Sets
+
+# Ingen ordning, tillåter inte dubletter
+
+# Går att lägga till och ta bort element, men inte byta ut eller ändra ett specifikt element
+
+# metoder för sets: https://www.w3schools.com/python/python_sets_methods.asp
+
+# issubset(), intersection(), difference()
