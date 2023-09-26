f6ab150b60a73e2a9632889beb4106bb40559e31
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2023-09-26 10:02:43 +0200

    grund för genomgång vecka 39

---
+++ T23-Vecks-39.py
@@ -0,0 +1,22 @@
+# Funktioner
+
+# Ett block med kod som kan användas på andra ställen i programmet
+
+# En funktion har ett namn och behöver definieras
+
+# För att köra en funktion behöver vi kalla på den. Först definierar vi bara den, sen kan vi använda den.
+# Vi kan använda den flera gånger om vi vill.
+
+# En funktion kan ta in en parameter som den kan använda. Det kan vara vilken slags variabel som helst.
+
+# Funktioner kan också ta in flera parametrar
+
+# Om man skriver in för många eller för få parametrar blir det fel
+
+# Vi kan ha en default också om vi vill, då behövs inte parametern
+
+# Funktionen kan returnera något
+
+# Om vi vill spara det som returneras måste vi lägga det i en variabel
+
+# Det går att returnera flera saker från en funktion.
