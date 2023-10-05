e9816d0d8a23974c92b81267367c027d2c20a9a8
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2023-10-05 12:15:31 +0200

    grund genomgång vecka 41

---
+++ T23-Vecka-41.py
@@ -0,0 +1,11 @@
+# Dictionary
+
+# Skrivs med {} och har par av nyckel och värde
+
+# Ordnade, går att ändra, tillåter inte dubletter (inte samma nyckel)
+
+# Skriva ut ett värde från en nyckel
+
+# Kan innehålla olika datatyper
+
+# Metoder för dictionary: https://www.w3schools.com/python/python_dictionaries_methods.asp
