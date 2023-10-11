0c74c0f4c0c8fac59bbc62dfefc5c57601c427f9
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2023-10-11 12:02:15 +0200

    anteckningar från vecka 41

--- T23-Vecka-41.py
+++ T23-Vecka-41.py
@@ -1,11 +1,19 @@
 # Dictionary
 
+ålder = {"Felicia":24,"Frida":22,"Maximiliam":15,"Victor":24}
+
 # Skrivs med {} och har par av nyckel och värde
 
 # Ordnade, går att ändra, tillåter inte dubletter (inte samma nyckel)
 
 # Skriva ut ett värde från en nyckel
+värde = ålder["Maximiliam"]
+print(värde)
 
 # Kan innehålla olika datatyper
+intressen = {"Felicia":["segla","baka","resa"],"Victor":["laga mat","hockey","spela spel"]}
 
 # Metoder för dictionary: https://www.w3schools.com/python/python_dictionaries_methods.asp
+
+ålder["Felicia"]= input("skriv en ålder: ")
+print(ålder["Felicia"])
